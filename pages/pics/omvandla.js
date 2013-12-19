25dd8818bbce88fe2ee4f559617e2ffc2a5330f2
OpenShift guest <sm222jc@student.lnu.se>
2013-12-19 05:40:55 -0500

    nästan klar med uppgift 2 valutaomvandlare

--- pages/pics/omvandla.js
+++ pages/pics/omvandla.js
@@ -3,16 +3,19 @@
 
 var input = document.querySelector("#kronor");
 var omvandla = document.querySelector("#convert");
+var valueconverted = document.getElementById("convertedValue");
 
 var dollar = 0.51;
 var euro = 0.11;
 
-omvandla.onclick = function (){
+omvandla.onclick = function (e){
     if(isNaN(input.value)){
         alert ("Fyll i siffror");
     }
     else{
-        //alert ("Siffror ifyllda")
+        valueconverted.innerHTML = "Dollar: " + input.value*dollar + " Euro: " + input.value*euro + "<br>" + valueconverted.innerHTML;
         console.log(("Dollar:" + input.value*dollar) + ("Euro:" + input.value*euro));
+        e.preventDefault(); 
+
     }
 };
